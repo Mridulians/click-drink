5f1a8f398343c7a73ffba7dfab5a7f3267333803
Mridulians <gmridul898@gmail.com>
2024-12-12 17:25:53 +0530

    backend updated

--- back/routes/clicks.js
+++ back/routes/clicks.js
@@ -2,13 +2,10 @@ const express = require("express");
 const ClicksSchema = require("../models/Clicks");
 const router = express.Router();
 
-
-
-
 // save data to the database
 router.post("/save", async (req, res) => {
   const { username, clicks, dollars } = req.body;
-  
+
   try {
     // Check if the user already exists in the database
     let user = await ClicksSchema.findOne({ username });
@@ -31,7 +28,22 @@ router.post("/save", async (req, res) => {
   }
 });
 
+// Fetch user data by username
+router.get("/getdata/:username", async (req, res) => {
+  const { username } = req.params;
 
+  try {
+    const user = await ClicksSchema.findOne({ username });
+    if (user) {
+      return res.status(200).json(user);
+    } else {
+      return res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    console.error("Error fetching user data:", error);
+    res.status(500).json({ message: "Error fetching data" });
+  }
+});
 
 // Fetch all data (Admin Panel)
 router.get("/getdata", async (req, res) => {
